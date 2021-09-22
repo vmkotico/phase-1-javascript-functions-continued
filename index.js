d6617c2904fde99c00e50a3a94f26a17a0308dfb
Vida May Kotico <vmkotico@yahoo.com>
2021-09-22 16:02:43 -0700

    commit tree 77c4a368ce8924b3492f2e7c38e8fdc8b8b70015

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`); 
+}
+
+
+
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+
+}
+
+
+
+function wrapAdjective (emphatic1 = "*", emphatic2 = "||") {
+
+}
+
+function result (wrapAdjective) {
+    return (`You are a!`); 
+
+}
+
+wrapAdjective(result); 
